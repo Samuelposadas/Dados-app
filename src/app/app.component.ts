e092a8325ef531b58664d5241e773b589b92f05b
Samuel Posadas <samugonza99@gmail.com>
2021-07-21 15:46:45 -0300

    mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados-app';
+  diceOne = "../assets/img/dice2.png";
+  diceTwo = "../assets/img/dice5.png";
+
+  numberOne = 1;
+  numberTwo = 2;
+
+  rollDices(): void {
+    this.numberOne = Math.round(Math.random() * 5) + 1;
+    this.numberTwo = Math.round(Math.random() * 5) + 1;
+
+    this.diceOne = `../assets/img/dice${this.numberOne}.png`
+    this.diceTwo = `../assets/img/dice${this.numberTwo}.png`
+  }
+
 }
